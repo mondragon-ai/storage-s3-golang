7cdc72adeec746616b3175d87b2e69798008a0cf
mondragon-ai <angel.mondragon@yahoo.com>
2025-01-03 12:13:18 -0300

    no caching & encoding a new name

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
 	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -29,7 +35,11 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 
 	const maxMemory = 10 << 20 // 10 MB
-	r.ParseMultipartForm(maxMemory)
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error parsing form data", err)
+		return
+	}
 
 	file, header, err := r.FormFile("thumbnail")
 	if err != nil {
@@ -38,38 +48,76 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 	defer file.Close()
 
-	mediaType := header.Header.Get("Content-Type")
-	if mediaType == "" {
+	contentType := header.Header.Get("Content-Type")
+	if contentType == "" {
 		respondWithError(w, http.StatusBadRequest, "Missing Content-Type for thumbnail", nil)
 		return
 	}
 
-	data, err := io.ReadAll(file)
+	// Parse the media type
+	mediaType, _, err := mime.ParseMediaType(contentType)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Error reading file", err)
+		respondWithError(w, http.StatusBadRequest, "Invalid Content-Type format", err)
 		return
 	}
+
+	// Validate allowed media types
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Unsupported file type. Only JPEG and PNG are allowed.", nil)
+		return
+	}
+
+	// Extract file extension based on media type
+	extension := ""
+	switch mediaType {
+	case "image/jpeg":
+		extension = ".jpg"
+	case "image/png":
+		extension = ".png"
+	}
+
+	// Generate a random file name
+	randomBytes := make([]byte, 32)
+	_, err = rand.Read(randomBytes)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to generate random filename", err)
+		return
+	}
+	randomFileName := base64.RawURLEncoding.EncodeToString(randomBytes)
+
+	// Construct the file path
+	filePath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s%s", randomFileName, extension))
+
+	// Create the file on the filesystem
+	outFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to create file on disk", err)
+		return
+	}
+	defer outFile.Close()
+
+	// Copy the file data to the new file
+	_, err = io.Copy(outFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to save file to disk", err)
+		return
+	}
+
+	// Construct the thumbnail URL
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s%s", cfg.port, randomFileName, extension)
 
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't find video", err)
 		return
 	}
+
 	if video.UserID != userID {
 		respondWithError(w, http.StatusUnauthorized, "Not authorized to update this video", nil)
 		return
 	}
 
-	// base64Encoded := base64.StdEncoding.EncodeToString(data)
-	// base64DataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, base64Encoded)
-
-	// video.ThumbnailURL = &base64DataURL
-
-	// TODO: 1. create the path
-	// TODO: 2. create file
-	// TODO: 3. Copy data from FormData -> new file
-	// TODO: 3. Save new file
-
+	video.ThumbnailURL = &thumbnailURL
 
 	err = cfg.db.UpdateVideo(video)
 	if err != nil {
